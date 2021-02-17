019097882468907c25e682db534d69030dd15eee
Alexandre Pereira <naitwa.alexandre@gmail.com>
2021-02-17 06:10:04 -0300

    first one

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { TextRotationDownSharp } from '@material-ui/icons';
+import React from 'react'
+
+const App = () => {
+    return (
+        <div>
+            E-commerce
+        </div>
+    )
+}
+
+export default App;
