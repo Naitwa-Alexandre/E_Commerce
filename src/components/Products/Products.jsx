49e1fdb6b860a7c8f013be6ff31b064b10e770be
Alexandre Pereira <naitwa.alexandre@gmail.com>
2021-02-18 08:58:20 -0300

    Products

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 
 import Product from './Product/Product';
+import useStyles from './styles';
 
-const products = [
-    {id: 1, name: 'Shoes', description: 'Running shoes.', price: '$5', image: 'https://cdn.hswstatic.com/gif/barefoot-run-shoe-1.jpg'},
-    {id: 2, name: 'Macbook', description: 'Apple macbook.', price: '$10', image: 'https://ibcdn.canaltech.com.br/VKcx7ohXhyUoi_aajZYRNf5pVrc=/fit-in/400x400/filters:fill(transparent):watermark(wm/prd.png,-32p,center,1,none,15)/i413119.png'},
-    {id: 3, name: 'Cup', description: 'Coffe Cup.', price: '$1.5', image: 'https://images-na.ssl-images-amazon.com/images/I/717jo4rq9AL._SL1500_.jpg'},
-];
 
-const Products = () => {
+
+const Products = ({ products }) => {
+    const classes = useStyles();
+
     return (
-        <main>
+        <main className={classes.content}>
+            <div className={classes.toolbar} />
             <Grid container justify="center" spacing={4}>
                 {products.map((product) => (
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
